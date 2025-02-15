f23c65ce547f04b2383bf585586d2a834e92757d
pp3906 <pp3906@srmist.edu.in>
2025-02-15 23:18:37 +0530

    Initial Commit

--- android/src/main/java/com/typestepcounter/TypeStepCounterModule.kt
+++ android/src/main/java/com/typestepcounter/TypeStepCounterModule.kt
@@ -1,25 +1,56 @@
 package com.typestepcounter
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
-import com.facebook.react.bridge.Promise
+import com.facebook.react.modules.core.DeviceEventManagerModule
 
 class TypeStepCounterModule(reactContext: ReactApplicationContext) :
-  ReactContextBaseJavaModule(reactContext) {
+  ReactContextBaseJavaModule(reactContext), SensorEventListener {
+
+  private var sensorManager: SensorManager? = null
+  private var stepCounterSensor: Sensor? = null
+
+  init {
+    sensorManager = reactContext.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+    stepCounterSensor = sensorManager?.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
+  }
 
   override fun getName(): String {
-    return NAME
+    return "TypeStepCounter"
   }
 
-  // Example method
-  // See https://reactnative.dev/docs/native-modules-android
   @ReactMethod
-  fun multiply(a: Double, b: Double, promise: Promise) {
-    promise.resolve(a * b)
+  fun startStepCounter() {
+    stepCounterSensor?.let {
+      sensorManager?.registerListener(this, it, SensorManager.SENSOR_DELAY_UI)
+    }
   }
 
-  companion object {
-    const val NAME = "TypeStepCounter"
+  @ReactMethod
+  fun stopStepCounter() {
+    sensorManager?.unregisterListener(this)
+  }
+
+  override fun onSensorChanged(event: SensorEvent?) {
+    if (event?.sensor?.type == Sensor.TYPE_STEP_COUNTER) {
+      val stepCount = event.values[0].toInt()
+      sendEvent("StepCounterUpdate", stepCount)
+    }
+  }
+
+  override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+    // Not needed for step counter
+  }
+
+  private fun sendEvent(eventName: String, stepCount: Int) {
+    reactApplicationContext
+      .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter::class.java)
+      .emit(eventName, stepCount)
   }
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import { NativeModules, Platform } from 'react-native';
+import { NativeModules, NativeEventEmitter, Platform } from 'react-native';
 
 const LINKING_ERROR =
   `The package 'react-native-type-step-counter' doesn't seem to be linked. Make sure: \n\n` +
@@ -6,17 +6,32 @@ const LINKING_ERROR =
   '- You rebuilt the app after installing the package\n' +
   '- You are not using Expo Go\n';
 
-const TypeStepCounter = NativeModules.TypeStepCounter
-  ? NativeModules.TypeStepCounter
-  : new Proxy(
-      {},
-      {
-        get() {
-          throw new Error(LINKING_ERROR);
-        },
-      }
-    );
+const { TypeStepCounter } = NativeModules;
 
-export function multiply(a: number, b: number): Promise<number> {
-  return TypeStepCounter.multiply(a, b);
+if (!TypeStepCounter) {
+  throw new Error(LINKING_ERROR);
 }
+
+const stepCounterEmitter = new NativeEventEmitter(TypeStepCounter);
+
+/**
+ * Start step counter listener
+ * @param callback - function that receives the step count updates
+ * @returns function to stop listening
+ */
+export const startStepCounter = (callback: (steps: number) => void) => {
+  TypeStepCounter.startStepCounter();
+  const subscription = stepCounterEmitter.addListener('StepCounterUpdate', callback);
+
+  return () => {
+    subscription.remove();
+    TypeStepCounter.stopStepCounter();
+  };
+};
+
+/**
+ * Stop step counter manually (if needed)
+ */
+export const stopStepCounter = () => {
+  TypeStepCounter.stopStepCounter();
+};
